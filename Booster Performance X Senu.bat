6514da77da7d2c56c1d95540f57a9c3b2edae995
SENU OPTIMIZE <senuthdamsuwa725@gmail.com>
2026-07-05 12:56:28 +0530

    Add files via upload

---
+++ Booster Performance X Senu.bat
@@ -0,0 +1,24 @@
+@echo off
+echo ===================================
+echo       Boosting Performance x Senu♾️
+echo ===================================
+
+echo.
+echo Deleting Temp files...
+del /s /q "%temp%\*.*"
+rd /s /q "%temp%" 2>nul
+mkdir "%temp%"
+
+echo.
+echo Bossting ...
+del /s /q "C:\Windows\Temp\*.*"
+for /d %%x in ("C:\Windows\Temp\*") do rd /s /q "%%x"
+
+echo.
+echo Boosing FPS 💠
+del /s /q "C:\Windows/Prefetch
+
+echo.
+echo    Boosted✅
+echo ===============
+pause
